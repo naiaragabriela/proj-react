237354f3f1a19f46a2330e0723d16b3304f4d084
naiaragabriela <naiara.costa@5by5.com.br>
2023-06-12 10:59:31 -0300

    test: ensure Login start with inputs with required state 2

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -9,7 +9,11 @@ describe ('Login Component', () => {
     expect(errorWrap.childElementCount).toBe(0)
     const submitButton = getByTestId('submit') as HTMLButtonElement
     expect(submitButton.disabled).toBe(true)
+    const emailStatus = getByTestId ('password-status')
+    expect(emailStatus.title).toBe('Campo Obrigatório')
+    expect(emailStatus.textContent).toBe('.')
     const passwordStatus = getByTestId ('password-status')
     expect(passwordStatus.title).toBe('Campo Obrigatório')
+    expect(passwordStatus.title).toBe('.')
     })
 })
