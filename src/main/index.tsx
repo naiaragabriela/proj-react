2b48876485ebbb960104284ddf053c18ea24893c
naiaragabriela <naiara.costa@5by5.com.br>
2023-06-13 17:19:55 -0300

    refactor: move factories to their own files

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -2,8 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Router } from '@/presentation/components'
 import '@/presentation/styles/global.scss'
+import { makelogin } from './factories/pages/login/login-factory'
 
 ReactDOM.render(
-  <Router />,
+  <Router
+  makeLogin={makelogin} 
+  />,
   document.getElementById('main')
 )
