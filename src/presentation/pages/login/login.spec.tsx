44cc0eb3fc08a3f1c5742533a8e88586ef8f5c05
naiaragabriela <naiara.costa@5by5.com.br>
2023-06-13 08:47:13 -0300

    test: ensure Login navigates to main page on success

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -17,7 +17,7 @@ type SutTypes = {
 type SutParams = {
   validationError: string
 }
-const history = createMemoryHistory()
+const history = createMemoryHistory({initialEntries: ['/login']})
 
 const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub()
@@ -171,6 +171,8 @@ describe ('Login Component', () => {
     simulateValidSubmit(sut)
     await waitFor(() => sut.getByTestId('form'))
     expect(localStorage.setItem).toHaveBeenCalledWith('accessToken',authenticationSpy.account.accessToken)
+    expect(history.length).toBe(1)
+    expect(history.location.pathname).toBe('/')
   })
 
   test ('Should go to signup page', () => {
