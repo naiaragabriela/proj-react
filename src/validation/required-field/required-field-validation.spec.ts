eb1f23254af9ab3ddb4ce93f9bce415e89644e52
naiaragabriela <naiara.costa@5by5.com.br>
2023-06-13 10:31:08 -0300

    test: ensure RequiredFieldValidation returns falsy if field is not empty

--- src/validation/required-field/required-field-validation.spec.ts
+++ src/validation/required-field/required-field-validation.spec.ts
@@ -1,5 +1,6 @@
 import { RequiredFieldError } from "@/validation/errors"
 import { RequiredFieldValidation } from "./required-field-validation"
+import faker from 'faker'
 
 
 describe('RequiredFieldValidation', () => {
@@ -7,5 +8,11 @@ describe('RequiredFieldValidation', () => {
     const sut = new RequiredFieldValidation('email')
     const error = sut.validate('')
     expect(error).toEqual(new RequiredFieldError())
+  })
+
+  test('Should return falsy if field is empty', () => {
+    const sut = new RequiredFieldValidation('email')
+    const error = sut.validate(faker.random.word())
+    expect(error).toBeFalsy()
   })
 })
