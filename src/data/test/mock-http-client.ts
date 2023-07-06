ff904162bdf086a1a477d6abd7b0d08cf9653848
naiaragabriela <naiara.costa@5by5.com.br>
2023-07-06 18:09:26 -0300

    refactor: folder fix

--- src/data/test/mock-http-client.ts
+++ src/data/test/mock-http-client.ts
@@ -2,15 +2,15 @@ import { HttpPostClient, HttpPostParams, HttpResponse, HttpStatusCode } from '@/
 
 
 export class HttpPostClientSpy<T, R> implements HttpPostClient<T, R> {
-    url?: string
-    body?: T
-    response: HttpResponse<R> = {
-      statusCode: HttpStatusCode.ok
-    }
+  url?: string
+  body?: T
+  response: HttpResponse<R> = {
+    statusCode: HttpStatusCode.ok
+  }
 
-    async post (params: HttpPostParams<T>): Promise<HttpResponse<R>> {
-      this.url = params.url
-      this.body = params.body
-      return Promise.resolve(this.response)
-    }
+  async post (params: HttpPostParams<T>): Promise<HttpResponse<R>> {
+    this.url = params.url
+    this.body = params.body
+    return Promise.resolve(this.response)
+  }
 }
