7bcd309bc15ae69e559684aab0b05cc7a2f27e6a
naiaragabriela <naiara.costa@5by5.com.br>
2023-07-24 16:04:09 -0300

    test: ensure SignUp change password status to error if validation fails

--- src/presentation/pages/singup/signup.spec.tsx
+++ src/presentation/pages/singup/signup.spec.tsx
@@ -36,7 +36,7 @@ describe ('SignUp Component', () => {
     Helper.testButtonIsDisabled(sut, 'submit', true)
     Helper.testStatusForField(sut, 'name', validationError)
     Helper.testStatusForField(sut, 'email', validationError)
-    Helper.testStatusForField(sut, 'password', 'Campo Obrigatório')
+    Helper.testStatusForField(sut, 'password', validationError)
     Helper.testStatusForField(sut, 'passwordConfirmation', 'Campo Obrigatório')
   })
 
@@ -52,5 +52,12 @@ describe ('SignUp Component', () => {
     const { sut } = makeSut({validationError})
     Helper.populateField(sut, 'email')
     Helper.testStatusForField(sut, 'email', validationError)
+  })
+
+  test ('Should show password error if Validation fails', () => {
+    const validationError = faker.random.words()
+    const { sut } = makeSut({validationError})
+    Helper.populateField(sut, 'password')
+    Helper.testStatusForField(sut, 'password', validationError)
   })
 })
