86c8f31176c0eb33efec8fcd1123a4b66919d002
naiaragabriela <naiara.costa@5by5.com.br>
2023-07-24 13:41:56 -0300

    test: ensure RemoteAddAccount return an AccountModel on sucess

--- src/data/usecases/add-account/remote-add-account.spec.ts
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -1,7 +1,7 @@
 import { RemoteAddAccount } from './remote-add-account'
 import { HttpPostClientSpy } from '@/data/test'
 import { HttpStatusCode } from '@/data/protocols/http'
-import { mockAddAccountParams  } from '@/domain/test'
+import { mockAccountModel, mockAddAccountParams  } from '@/domain/test'
 import { AccountModel } from '@/domain/models'
 import { AddAccountParams } from '@/domain/usecases'
 import { EmailInUseError, UnexpectedError} from '@/domain/errors'
@@ -69,6 +69,17 @@ describe('RemoteAddAccount', () => {
         }
         const promise = sut.add(mockAddAccountParams())
         await expect(promise).rejects.toThrow(new UnexpectedError())
+      })
+
+      test('Should return an AccountModel if HttpPostClient returns 200', async () => {
+        const { sut, httpPostClientSpy } = makeSut()
+        const httpResult = mockAccountModel()
+        httpPostClientSpy.response = {
+          statusCode: HttpStatusCode.ok,
+          body: httpResult
+        }
+        const account = await sut.add(mockAddAccountParams())
+        expect(account).toEqual(httpResult)
       })
     
 
