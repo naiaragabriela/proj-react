48e8103993b926fdaafd2535a4ff8fa3d3963f92
naiaragabriela <naiara.costa@5by5.com.br>
2023-07-25 11:48:39 -0300

    test ensure SignUp present error if SaveAccessToken fails

--- src/presentation/pages/singup/signup.spec.tsx
+++ src/presentation/pages/singup/signup.spec.tsx
@@ -183,4 +183,13 @@ describe ('SignUp Component', () => {
     expect(history.location.pathname).toBe('/')
   })
 
+  test ('Should present error if SaveAccessToken fails', async () => {
+    const { sut, saveAccessTokenMock } = makeSut()
+    const error = new EmailInUseError()
+    jest.spyOn(saveAccessTokenMock, 'save').mockRejectedValueOnce(error)
+    await simulateValidSubmit(sut)
+    Helper.testElementText(sut, 'main-error',error.message)
+    Helper.testChildCount(sut, 'error-wrap', 1)
+  })
+
 })
