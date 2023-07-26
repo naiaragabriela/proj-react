f9cf388a503123a78981e6bf1dab2e6dd6ab5a08
naiaragabriela <naiara.costa@5by5.com.br>
2023-07-26 11:51:29 -0300

    test: ensure Login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,7 +1,14 @@
 describe('Login', () => {
-    it('Should load with correct initial state', () => {
-        cy.visit('login')
-    })
+  beforeEach(() => {
+    cy.visit('login')
+  })
+  
+  it('Should load with correct initial state', () => {
+    cy.getByTestId('email-status').should('have.attr', 'title', 'Campo Obrigatório')
+    cy.getByTestId('password-status').should('have.attr', 'title', 'Campo Obrigatório')
+    cy.getByTestId('submit').should('have.attr', 'disabled') 
+    cy.getByTestId('error-wrap').should('not.have.descendants') 
 })
 
+})
       
