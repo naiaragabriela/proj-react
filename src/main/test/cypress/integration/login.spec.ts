1e9a6ce9b475dd4678b56a0c1a31757a216d2580
naiaragabriela <naiara.costa@5by5.com.br>
2023-07-26 12:05:50 -0300

    test: ensure login present valid state if form is valid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -21,5 +21,14 @@ describe('Login', () => {
     cy.getByTestId('error-wrap').should('not.have.descendants') 
   })
 
+  it('Should present valid state if form is valid', () => {
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('email-status').should('have.attr', 'title', 'OK')
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
+    cy.getByTestId('password-status').should('have.attr', 'title', 'OK')
+    cy.getByTestId('submit').should('not.have.attr', 'disabled') 
+    cy.getByTestId('error-wrap').should('not.have.descendants') 
+  })
+
 })
       
