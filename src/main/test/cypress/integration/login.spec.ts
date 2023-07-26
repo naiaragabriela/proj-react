c37129530353466821f1ece8abcaba01bb3a9f98
naiaragabriela <naiara.costa@5by5.com.br>
2023-07-26 12:02:41 -0300

    test: ensure login present error state if form is invalid

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,3 +1,5 @@
+import faker from 'faker'
+
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('login')
@@ -8,7 +10,16 @@ describe('Login', () => {
     cy.getByTestId('password-status').should('have.attr', 'title', 'Campo Obrigatório')
     cy.getByTestId('submit').should('have.attr', 'disabled') 
     cy.getByTestId('error-wrap').should('not.have.descendants') 
-})
+  })
+
+  it('Should present error state if form is invalid', () => {
+    cy.getByTestId('email').focus().type(faker.random.word())
+    cy.getByTestId('email-status').should('have.attr', 'title', 'Valor inválido')
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(3))
+    cy.getByTestId('password-status').should('have.attr', 'title', 'Valor inválido')
+    cy.getByTestId('submit').should('have.attr', 'disabled') 
+    cy.getByTestId('error-wrap').should('not.have.descendants') 
+  })
 
 })
       
