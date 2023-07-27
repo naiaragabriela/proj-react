aeb2eaecfc38a0b5a04923774edce193d61fc132
naiaragabriela <naiara.costa@5by5.com.br>
2023-07-27 14:44:06 -0300

    refactor: improve input layout

--- src/presentation/test/form-helper.tsx
+++ src/presentation/test/form-helper.tsx
@@ -21,11 +21,14 @@ export const testButtonIsDisabled = (
 export const testStatusForField = (
     sut: RenderResult, 
     fieldName: string, 
-    validationError?: string ): void => {
+    validationError: string = '' ): void => {
   
-    const fieldStatus = sut.getByTestId(`${fieldName}-status`)
-    expect(fieldStatus.title).toBe(validationError || 'OK')
-    expect(fieldStatus.textContent).toBe(validationError? '.' : '')
+    const wrap = sut.getByTestId(`${fieldName}-wrap`)
+    const field = sut.getByTestId(fieldName)
+    const label = sut.getByTestId(`${fieldName}-label`)
+    expect(wrap.getAttribute('data-status')).toBe(validationError ? 'invalid' : 'valid')
+    expect(field.title).toBe(validationError)
+    expect(label.title).toBe(validationError)
 }
 
 export const populateField = (sut: RenderResult, fieldName: string, value = faker.random.word()): void => {
