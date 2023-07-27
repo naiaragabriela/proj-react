ffe3782ab19d720b3ca9481ac6944decda448541
naiaragabriela <naiara.costa@5by5.com.br>
2023-07-27 13:45:26 -0300

    refactor: change test to use faker

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -37,29 +37,19 @@ describe('Login', () => {
     cy.getByTestId('email').focus().type(faker.internet.email())
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
     cy.getByTestId('submit').click()
-    cy.getByTestId('error-wrap')
-    /*
-    .getByTestId('spinner').should('exist')
-    .getByTestId('main-error').should('not.exist')
-    .getByTestId('spinner').should('not.exist')
-    .getByTestId('main-error').should('contain.text', 'Credenciais inválidas')
-    */
-   cy.url().should('eq', `${baseUrl}/login`)
+    cy.getByTestId('spinner').should('not.exist')
+    cy.getByTestId('main-error').should('contain.text', 'Algo de errado aconteceu. Tente novamente em breve')
+    cy.url().should('eq', `${baseUrl}/login`)
   })
 
   it('Should present save accessToken if valid credentials are provided', () => {
-    cy.getByTestId('email').focus().type('jose@hotmail.com')
-    cy.getByTestId('password').focus().type('12345')
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(5))
     cy.getByTestId('submit').click()
-    cy.getByTestId('error-wrap')
-    /*
-    .getByTestId('spinner').should('exist')
-    .getByTestId('main-error').should('not.exist')
-    .getByTestId('spinner').should('not.exist')
-    .getByTestId('main-error').should('contain.text', 'Credenciais inválidas')
-    */
-   //cy.url().should('eq', `${baseUrl}/`)
-   //cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
+    cy.getByTestId('main-error').should('not.exist')
+    cy.getByTestId('spinner').should('not.exist')
+    cy.url().should('eq', `${baseUrl}/`)
+    cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
   })
 })
       
