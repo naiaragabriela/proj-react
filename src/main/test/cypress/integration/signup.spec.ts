f2aae0e081112c28db3702ffbe4b7ac4685b038e
naiaragabriela <naiara.costa@5by5.com.br>
2023-07-28 12:02:05 -0300

    test: ensure SignUp prevent submit if form is invalid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -97,4 +97,10 @@ describe('SignUp', () => {
         cy.getByTestId('submit').dblclick()
          //testHttpCallsCount(1)
     })
+
+    it('Should not call submit if form is invalid', () => {
+        Http.mockOk()
+        cy.getByTestId('email').focus().type(faker.internet.email()).type('{enter}')
+        //testHttpCallsCount(0)
+    })
 })
