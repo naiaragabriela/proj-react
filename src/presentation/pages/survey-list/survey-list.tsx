32d7a90e05084292e4f0cd2f616897e1a4502421
naiaragabriela <naiara.costa@5by5.com.br>
2023-07-31 11:26:10 -0300

    feat: add skeleton shimmer effect

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -21,6 +21,7 @@ const SurveyList: React.FC = () => {
                     </div>
                     <footer>Ver Resultado</footer>
                 </li>
+                <li></li>
               </ul>
             </div>
             <Footer />
