06601c9f6d8ed4081d5bcb3452f80e168f8fbe0c
naiaragabriela <naiara.costa@5by5.com.br>
2023-07-31 19:39:32 -0300

    feat:  ensure PrivateRoute renders component if token is not empty

--- src/presentation/contexts/api/api-context.ts
+++ src/presentation/contexts/api/api-context.ts
@@ -4,7 +4,6 @@ import { createContext } from 'react'
 type Props = {
  setCurrentAccount?: (account: AccountModel) => void
  getCurrentAccount?: () => AccountModel
-
 }
 
 export default createContext<Props>(null)
