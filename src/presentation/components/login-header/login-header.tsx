9f5b2c93aef7c31aa4fc69c96b33436c7fb4bfd9
naiaragabriela <naiara.costa@5by5.com.br>
2023-07-31 10:07:20 -0300

    refactor: adapt layout to mobile

--- src/presentation/components/login-header/login-header.tsx
+++ src/presentation/components/login-header/login-header.tsx
@@ -4,7 +4,7 @@ import Logo from '@/presentation/components/logo/logo'
 
 const LoginHeader: React.FC = () => {
  return (
-    <header className={Styles.header}>
+    <header className={Styles.headerWrap}>
     <Logo />
     <h1>React and Clean Arquiteture</h1>
   </header>
