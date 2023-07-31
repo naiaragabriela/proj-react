6df075f7cee7232193831b6e7c95f068f0832308
naiaragabriela <naiara.costa@5by5.com.br>
2023-07-31 14:10:52 -0300

    refactor: move Router to main layer

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,14 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router } from '@/presentation/components'
+import  Router  from './routes/router'
 import '@/presentation/styles/global.scss'
-import { makeLogin } from '@/main/factories/pages/login/login-factory'
-import { makeSignUp } from './factories/pages/signup/signup-factory'
+
 
 ReactDOM.render(
-  <Router
-  makeLogin={makeLogin} 
-  makeSignUp={makeSignUp}
-  />,
+  <Router />,
   document.getElementById('main')
 )
---
+++ src/main/routes/router.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import { makeLogin } from '@/main/factories/pages/login/login-factory'
+import { makeSignUp } from '@/main/factories/pages/signup/signup-factory'
+
+import { SurveyList } from '@/presentation/pages'
+
+const Router: React.FC= () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" exact component={makeLogin} />
+        <Route path="/signup" exact component={makeSignUp} />
+        <Route path="/" exact component={SurveyList} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+   
+export default Router
