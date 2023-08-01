e4ef88a2523b3b9f00d05da4868f80db714a38c1
naiaragabriela <naiara.costa@5by5.com.br>
2023-08-01 17:49:29 -0300

    refactor: move SUT creation to a factory method

--- src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
+++ src/main/decorators/authorize-http-get-client-decorator/authorize-http-get-client-decorator.spec.ts
@@ -1,8 +1,28 @@
 import { mockGetRequest, GetStorageSpy } from "@/data/test"
 import { AuthorizedHttpGetClientDecorator } from "./authorize-http-get-client-decorator"
 
+type SutTypes = {
+  sut: AuthorizedHttpGetClientDecorator
+  getStorageSpy: GetStorageSpy
+}
+
+const makeSut = (): SutTypes => {
+    const getStorageSpy = new GetStorageSpy()
+    const sut = new AuthorizedHttpGetClientDecorator(getStorageSpy)
+    return {
+        sut,
+        getStorageSpy
+    }
+}
+
 describe('AuthorizedHttpGetClientDecorator', () => { 
     test('Should call GetStorage with correct value', () => {
+        const { sut, getStorageSpy } = makeSut()
+        sut.get(mockGetRequest())
+        expect(getStorageSpy.key).toBe('account')
+    })
+
+    test('Should not add headers if getStorage is invalid', () => {
         const getStorageSpy = new GetStorageSpy()
         const sut = new AuthorizedHttpGetClientDecorator(getStorageSpy)
         sut.get(mockGetRequest())
