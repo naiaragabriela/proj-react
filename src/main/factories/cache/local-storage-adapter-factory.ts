d497ffb92147e92cb7d912d70187666a7cda65b2
naiaragabriela <naiara.costa@5by5.com.br>
2023-08-02 09:45:08 -0300

    feat: ensure LocalStorageAdapter calss remoteItem if value is null

--- src/main/factories/cache/local-storage-adapter-factory.ts
+++ src/main/factories/cache/local-storage-adapter-factory.ts
@@ -1,4 +1,4 @@
-import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter';
+import { LocalStorageAdapter } from '@/infra/cache/local-storage-adapter/local-storage-adapter';
 
 export const makeLocalStorageAdapter = (): LocalStorageAdapter => {
     return new LocalStorageAdapter()
