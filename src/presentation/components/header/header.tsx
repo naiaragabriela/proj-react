b372767dac1b6bf42f55fa22afac4c81573c9561
naiaragabriela <naiara.costa@5by5.com.br>
2023-08-02 10:05:14 -0300

    feat: ensure logout calls setCurrentAccount with undefined

--- src/presentation/components/header/header.tsx
+++ src/presentation/components/header/header.tsx
@@ -1,15 +1,24 @@
-import React, { memo } from 'react'
+import React, { memo, useContext } from 'react'
 import  Styles from './header-styles.scss'
+import { ApiContext } from '@/presentation/contexts'
+import { useHistory } from 'react-router-dom'
 import Logo from '../logo/logo'
 
 const Header: React.FC = () => {
+  const history = useHistory()
+  const  { setCurrentAccount } = useContext(ApiContext)
+  const logout= (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>): void => {
+    event.preventDefault()
+    setCurrentAccount(undefined)
+    history.replace('/login')
+  }
     return (
         <header className={Styles.headerWrap}>
         <div className={Styles.headerContent}>
            <Logo />
            <div className={Styles.logoutWrap}>
              <span>Naiara</span>
-             <a href="#">Sair</a>
+             <a data-testid="logout" href="#" onClick={logout}>Sair</a>
            </div>
         </div> 
      </header>
