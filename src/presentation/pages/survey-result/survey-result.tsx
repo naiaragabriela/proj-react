79dcb001b5c4cf9a546bd7e4b602024fd8490d82
naiaragabriela <naiara.costa@5by5.com.br>
2023-08-03 19:37:11 -0300

    feat: add skeleton with shimmer or SurveyResult

--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -15,17 +15,7 @@ const SurveyResult: React.FC = () => {
             <h2>Pergunta: qual seu framework favorito? </h2>
           </hgroup>
           <FlipMove className={Styles.answerList}>
-            <li>
-              <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freecodecamp.org%2Fportuguese%2Fnews%2Freact-js-para-iniciantes-props-e-state-explicados%2F&psig=AOvVaw0ItCi48mPTl3U-Df1vFIic&ust=1691169488728000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCKjM47r_wIADFQAAAAAdAAAAABAw" />
-              <span className={Styles.answer}>Resposta: React</span>
-              <span className={Styles.percent}>50%</span>
-            </li>
             <li className={Styles.active}>
-              <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freecodecamp.org%2Fportuguese%2Fnews%2Freact-js-para-iniciantes-props-e-state-explicados%2F&psig=AOvVaw0ItCi48mPTl3U-Df1vFIic&ust=1691169488728000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCKjM47r_wIADFQAAAAAdAAAAABAw" />
-              <span className={Styles.answer}>Resposta: React</span>
-              <span className={Styles.percent}>50%</span>
-            </li>
-            <li>
               <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freecodecamp.org%2Fportuguese%2Fnews%2Freact-js-para-iniciantes-props-e-state-explicados%2F&psig=AOvVaw0ItCi48mPTl3U-Df1vFIic&ust=1691169488728000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCKjM47r_wIADFQAAAAAdAAAAABAw" />
               <span className={Styles.answer}>Resposta: React</span>
               <span className={Styles.percent}>50%</span>
