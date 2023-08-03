2ed9ea5659eae466e5c63f90fdcc8e2270cd765e
naiaragabriela <naiara.costa@5by5.com.br>
2023-08-03 18:29:20 -0300

    refactor: move Calendar to its own Component

--- src/presentation/pages/survey-list/components/item/item.spec.tsx
+++ src/presentation/pages/survey-list/components/item/item.spec.tsx
@@ -13,26 +13,18 @@ describe('SurveyItem Component', () => {
     test('Should render with correct values', () => {
       const survey = Object.assign(mockSurveyModel(), {
           didAnswer: true,
-          date: new Date('2023-08-01T00:00:00')
       })
       makeSut(survey)
       expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbUp )
       expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
-      expect(screen.getByTestId('day')).toHaveTextContent('01')
-      expect(screen.getByTestId('month')).toHaveTextContent('ago')
-      expect(screen.getByTestId('year')).toHaveTextContent('2023')
     })
 
     test('Should render with correct values', () => {
         const survey = Object.assign(mockSurveyModel(), {
             didAnswer: false,
-            date: new Date('2019-05-03T00:00:00')
         })
         makeSut(survey)
         expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbDown )
         expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
-        expect(screen.getByTestId('day')).toHaveTextContent('03')
-        expect(screen.getByTestId('month')).toHaveTextContent('mai')
-        expect(screen.getByTestId('year')).toHaveTextContent('2019')
       })
 })
--- src/presentation/pages/survey-list/components/item/item.tsx
+++ src/presentation/pages/survey-list/components/item/item.tsx
@@ -1,7 +1,8 @@
 import Styles from './item-styles.scss'
-import { IconName, Icon } from '../../../../components'
+import { IconName, Icon, Calendar } from '../../../../components'
 import React from 'react'
 import { LoadSurveyList } from '../../../../../domain/usecases/load-survey-list'
+
 
 type Props = {
   survey: LoadSurveyList.Model
@@ -13,17 +14,7 @@ const SurveyItem: React.FC<Props> = ({ survey }: Props) => {
     <li className={Styles.surveyItemWrap}>
       <div className={Styles.surveyContent}>
         <Icon className={Styles.iconWrap} iconName={iconName}/>
-        <time>
-          <span data-testid="day" className={Styles.day}>
-            {survey.date.getDate().toString().padStart(2, '0')}
-          </span>
-          <span data-testid="month" className={Styles.month}>
-            {survey.date.toLocaleString('pt-BR', {month: 'short'}).replace('.', '')}
-          </span>
-          <span data-testid="year" className={Styles.year}>
-            {survey.date.getFullYear()}
-          </span>
-        </time>
+        <Calendar date={survey.date} className={Styles.calendarWrap} />
         <p data-testid="question">{survey.question}</p>
       </div>
       <footer>Ver Resultado</footer>
