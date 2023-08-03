280522bba3170c83e83bc5e6b521b7b7ef4e9ba1
naiaragabriela <naiara.costa@5by5.com.br>
2023-08-03 11:47:59 -0300

    feat: ensure RemoteLoadSurveyResult throws AccessDeniedError on 403

--- src/data/usecases/load-survey-result/remote-load-result.ts
+++ src/data/usecases/load-survey-result/remote-load-result.ts
@@ -1,11 +1,17 @@
-import { HttpGetClient } from "@/data/protocols/http";
+import { HttpGetClient, HttpStatusCode } from "@/data/protocols/http";
+import { AccessDeniedError } from "@/domain/errors";
 
 export class RemoteLoadSurveyResult {
     constructor (
         private readonly url: string,
         private readonly httpGetClient: HttpGetClient
     ) {}
+
     async load (): Promise<void> {
-      await this.httpGetClient.get({ url: this.url })
+      const httpResponse = await this.httpGetClient.get({ url: this.url })
+      switch (httpResponse.statusCode) {
+        case HttpStatusCode.ok: break 
+        default: throw new AccessDeniedError()
+      }
     }
 }
