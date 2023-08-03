379615fce5c0b50c795238357b65618e1879e5b0
naiaragabriela <naiara.costa@5by5.com.br>
2023-08-03 20:53:04 -0300

    feat: ensure SurveyResult presents correct initial state

--- src/presentation/components/private-route/private-route.tsx
+++ src/presentation/components/private-route/private-route.tsx
@@ -2,12 +2,18 @@ import React, { useContext } from 'react'
 import { Redirect, Route, RouteProps } from 'react-router-dom'
 import { ApiContext } from '../../contexts'
 
+
 const PrivateRoute: React.FC<RouteProps>= (props: RouteProps) => {
 const { getCurrentAccount } = useContext(ApiContext);
+/*return <Route {...props} />
+}*/
+
 return getCurrentAccount?.()?.accessToken
+
     ? <Route {...props} />
-    : <Route {...props} component={() => <Redirect to="/login" />} />
+    : <Route {...props} component={() => <Redirect to="/login" />} /> 
 
 }
+
 
 export default PrivateRoute
--- src/presentation/pages/survey-result/survey-result.tsx
+++ src/presentation/pages/survey-result/survey-result.tsx
@@ -1,38 +1,50 @@
 import Header from '@/presentation/components/header/header'
 import Footer from '@/presentation/components/footer/footer'
-import { Calendar, Loading } from '@/presentation/components'
+import { Calendar, Loading , Error} from '@/presentation/components'
+import { LoadSurveyResult } from '@/domain/usecases'
 import Styles from './survey-result-styles.scss'
 import FlipMove from 'react-flip-move'
-import React from 'react'
+import React, { useState } from 'react'
 
 const SurveyResult: React.FC = () => {
+  const [state] = useState({
+    isLoading: false,
+    error: '',
+    surveyResult: null as LoadSurveyResult.Model
+  })
+
     return (
       <div className={Styles.surveyResultWrap}>
         <Header />
-        <div className={Styles.contentWrap}>
-          <hgroup>
-            <Calendar date={new Date()} className={Styles.calendarWrap} />
-            <h2>Pergunta: qual seu framework favorito? </h2>
-          </hgroup>
-          <FlipMove className={Styles.answerList}>
-            <li>
-              <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freecodecamp.org%2Fportuguese%2Fnews%2Freact-js-para-iniciantes-props-e-state-explicados%2F&psig=AOvVaw0ItCi48mPTl3U-Df1vFIic&ust=1691169488728000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCKjM47r_wIADFQAAAAAdAAAAABAw" />
-              <span className={Styles.answer}>Resposta: React</span>
-              <span className={Styles.percent}>50%</span>
-            </li>
-            <li className={Styles.active}>
-              <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freecodecamp.org%2Fportuguese%2Fnews%2Freact-js-para-iniciantes-props-e-state-explicados%2F&psig=AOvVaw0ItCi48mPTl3U-Df1vFIic&ust=1691169488728000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCKjM47r_wIADFQAAAAAdAAAAABAw" />
-              <span className={Styles.answer}>Resposta: React</span>
-              <span className={Styles.percent}>50%</span>
-            </li>
-            <li className={Styles.active}>
-              <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freecodecamp.org%2Fportuguese%2Fnews%2Freact-js-para-iniciantes-props-e-state-explicados%2F&psig=AOvVaw0ItCi48mPTl3U-Df1vFIic&ust=1691169488728000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCKjM47r_wIADFQAAAAAdAAAAABAw" />
-              <span className={Styles.answer}>Resposta: React</span>
-              <span className={Styles.percent}>50%</span>
-            </li>
-          </FlipMove>
-          <button>Voltar</button> 
-          <Loading />
+        <div data-testid="survey-result" className={Styles.contentWrap}>
+          { state.surveyResult && 
+            <>
+              <hgroup> 
+                <Calendar date={new Date()} className={Styles.calendarWrap} />
+                <h2>Pergunta: qual seu framework favorito? </h2>
+              </hgroup>
+              <FlipMove className={Styles.answerList}>
+                <li>
+                  <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freecodecamp.org%2Fportuguese%2Fnews%2Freact-js-para-iniciantes-props-e-state-explicados%2F&psig=AOvVaw0ItCi48mPTl3U-Df1vFIic&ust=1691169488728000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCKjM47r_wIADFQAAAAAdAAAAABAw" />
+                  <span className={Styles.answer}>Resposta: React</span>
+                  <span className={Styles.percent}>50%</span>
+                </li>
+                <li className={Styles.active}>
+                  <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freecodecamp.org%2Fportuguese%2Fnews%2Freact-js-para-iniciantes-props-e-state-explicados%2F&psig=AOvVaw0ItCi48mPTl3U-Df1vFIic&ust=1691169488728000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCKjM47r_wIADFQAAAAAdAAAAABAw" />
+                  <span className={Styles.answer}>Resposta: React</span>
+                  <span className={Styles.percent}>50%</span>
+                </li>
+                <li className={Styles.active}>
+                  <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.freecodecamp.org%2Fportuguese%2Fnews%2Freact-js-para-iniciantes-props-e-state-explicados%2F&psig=AOvVaw0ItCi48mPTl3U-Df1vFIic&ust=1691169488728000&source=images&cd=vfe&opi=89978449&ved=0CA4QjRxqFwoTCKjM47r_wIADFQAAAAAdAAAAABAw" />
+                  <span className={Styles.answer}>Resposta: React</span>
+                  <span className={Styles.percent}>50%</span>
+                </li>
+              </FlipMove>
+              <button>Voltar</button> 
+            </>
+          }
+          { state.isLoading && <Loading /> }
+          { state.error && <Error error={state.error} reload={() => {}} /> }
         </div>
         <Footer /> 
       </div>
