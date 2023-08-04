2e923f66e325d36832f5b05c52b2e69b67a45258
naiaragabriela <naiara.costa@5by5.com.br>
2023-08-04 17:49:48 -0300

    feat: ensure SurveyList redirect to SurveyResult

--- src/presentation/pages/survey-list/components/item/item.tsx
+++ src/presentation/pages/survey-list/components/item/item.tsx
@@ -2,6 +2,7 @@ import Styles from './item-styles.scss'
 import { IconName, Icon, Calendar } from '../../../../components'
 import React from 'react'
 import { LoadSurveyList } from '../../../../../domain/usecases/load-survey-list'
+import { Link } from 'react-router-dom'
 
 
 type Props = {
@@ -17,7 +18,11 @@ const SurveyItem: React.FC<Props> = ({ survey }: Props) => {
         <Calendar date={survey.date} className={Styles.calendarWrap} />
         <p data-testid="question">{survey.question}</p>
       </div>
-      <footer>Ver Resultado</footer>
+      <footer>
+        <Link data-testid="link" to={`/surveys/${survey.id}`}>
+          Ver Resultado
+        </Link>
+      </footer>
     </li>
   )
 }
