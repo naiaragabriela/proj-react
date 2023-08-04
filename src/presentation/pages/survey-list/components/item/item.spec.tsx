112527dc2dfd07d3e0924b386419842c2a533509
naiaragabriela <naiara.costa@5by5.com.br>
2023-08-04 17:50:00 -0300

    test: ensure SurveyList redirect to SurveyResult

--- src/presentation/pages/survey-list/components/item/item.spec.tsx
+++ src/presentation/pages/survey-list/components/item/item.spec.tsx
@@ -1,12 +1,26 @@
 import { SurveyItem } from '..'
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import React from 'react'
 import { mockSurveyModel } from '../../../../../domain/test'
 import { IconName } from '../../../../components'
+import { Router } from 'react-router-dom'
+import { createMemoryHistory, MemoryHistory } from 'history'
+
+type SutTypes = {
+    history: MemoryHistory
+}
 
 
-const makeSut = (survey = mockSurveyModel()): void => {
-    render(<SurveyItem survey={survey} />)
+const makeSut = (survey = mockSurveyModel()): SutTypes => {
+    const history = createMemoryHistory({ initialEntries: ['/']})
+    render(
+    <Router history={history}>
+        <SurveyItem survey={survey} />
+    </Router>
+    )
+    return {
+        history
+    }
 }
 
 describe('SurveyItem Component', () => {
@@ -36,16 +50,11 @@ describe('SurveyItem Component', () => {
         expect(screen.getByTestId('year')).toHaveTextContent('2019')
       })
 
-      test('Should render with correct values', () => {
-        const survey = Object.assign(mockSurveyModel(), {
-            didAnswer: false,
-            date: new Date('2019-05-03T00:00:00')
-        })
-        makeSut(survey)
-        expect(screen.getByTestId('icon')).toHaveProperty('src', IconName.thumbDown )
-        expect(screen.getByTestId('question')).toHaveTextContent(survey.question)
-        expect(screen.getByTestId('day')).toHaveTextContent('03')
-        expect(screen.getByTestId('month')).toHaveTextContent('mai')
-        expect(screen.getByTestId('year')).toHaveTextContent('2019')
+      test('Should go to SurveyResult', () => {
+        const survey = mockSurveyModel()
+        fireEvent.click(screen.getByTestId('link'))
+        const { history } = makeSut(survey)
+        expect(history.location.pathname).toBe(`/surveys/${survey.id}`)
       })
+      
 })
