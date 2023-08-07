42c03c0f01ba81b66425ea66a546a869feff69a3
naiaragabriela <naiara.costa@5by5.com.br>
2023-08-07 19:58:37 -0300

    test: ensure RemoteSaveSurveyResult returns AccessDeniedError on 404

--- src/data/usecases/save-survey-result/remote-save-result.spec.ts
+++ src/data/usecases/save-survey-result/remote-save-result.spec.ts
@@ -2,7 +2,7 @@ import { HttpClientSpy, mockRemoteSurveyResultModel } from '@/data/test'
 import { HttpStatusCode } from '@/data/protocols/http'
 import { RemoteSaveSurveyResult } from '@/data/usecases'
 import { mockSaveSurveyResultParams } from '@/domain/test'
-import { AccessDeniedError } from '@/domain/errors'
+import { AccessDeniedError, UnexpectedError } from '@/domain/errors'
 import faker from 'faker'
 
 
@@ -45,4 +45,14 @@ describe('RemoteSaveSurveyResult', () => {
         const promise = sut.save(mockSaveSurveyResultParams())
         await expect(promise).rejects.toThrow(new AccessDeniedError())
     })
+
+    test('Should throw UnexpectedError if HttpClient returns 404', async () => {
+        const { sut, httpClientSpy } = makeSut()
+        httpClientSpy.response = {
+            statusCode: HttpStatusCode.notFound
+        }
+        const promise = sut.save(mockSaveSurveyResultParams())
+        await expect(promise).rejects.toThrow(new UnexpectedError())
+    })
+
 })
