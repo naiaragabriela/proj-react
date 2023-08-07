82d532e38bebf058827eee6b6c56feca1a2a4f62
naiaragabriela <naiara.costa@5by5.com.br>
2023-08-07 12:32:48 -0300

    feat: SaveSurveyResult useCase

--- src/domain/usecases/load-survey-result.ts
+++ src/domain/usecases/load-survey-result.ts
@@ -1,19 +1,11 @@
+import { SurveyResultModel } from "@/domain/models"
+
 export interface LoadSurveyResult {
   load: () => Promise<LoadSurveyResult.Model>
 }
 
 export namespace LoadSurveyResult {
-  export type Model = { 
-    question: string
-    date: Date
-    answers: Array<{
-      image?: string
-      answer: string
-      count: number
-      percent: number
-      isCurrentAccountAnswer: boolean
-    }>
-  }
+  export type Model = SurveyResultModel
 }
 
 
--- src/presentation/pages/survey-result/components/answer/answer.tsx
+++ src/presentation/pages/survey-result/components/answer/answer.tsx
@@ -1,15 +1,11 @@
+import { SurveyResultAnswerModel } from '@/domain/models'
 import Styles from './answer-styles.scss'
 import React from 'react'
 
-type Props = {
-  answer: {
-    image?: string
-    answer: string
-    count: number
-    percent: number
-    isCurrentAccountAnswer: boolean
-  }
+type Props =  {
+  answer: SurveyResultAnswerModel
 }
+  
 
 const Answer: React.FC<Props> = ({ answer }: Props) => {
   const activeClassName = answer.isCurrentAccountAnswer ? Styles.active : ''
